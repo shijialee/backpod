356029e21754811e080ec7baa20833d652269563
qiang <shijialee@gmail.com>
2021-11-11 06:10:22 +0000

    implemented get wayback snapshot urls

---
+++ session.py
@@ -0,0 +1,47 @@
+from settings import DEFAULT_USER_AGENT
+import requests
+import time
+import logging
+logger = logging.getLogger(__name__)
+
+class Session(object):
+    def __init__(self,
+        follow_redirects=False,
+        user_agent=DEFAULT_USER_AGENT,
+        max_retries=3,
+    ):
+        self.follow_redirects = follow_redirects
+        self.user_agent = user_agent
+        self.max_retries = max_retries
+
+    def get(self, url, **kwargs):
+        headers = {
+            "User-Agent": self.user_agent,
+        }
+        response_is_final = False
+        retries = 0
+        while (response_is_final == False):
+            res = requests.get(
+                url,
+                allow_redirects=self.follow_redirects,
+                headers=headers,
+                stream=True,
+                **kwargs
+            )
+
+            if res.status_code != 200:
+                logger.info("HTTP status code: {0}".format(res.status_code))
+
+            if int(res.status_code / 100) in [ 4, 5 ]: # 4XX and 5XX codes
+                logger.info("Waiting 1 second before retrying.")
+                retries += 1
+                if retries <= self.max_retries:
+                    logger.info("Waiting 1 second before retrying.")
+                    time.sleep(1)
+                    continue
+                else:
+                    logger.info("Maximum retries reached, skipping.")
+                    return None
+            else:
+                response_is_final = True
+        return res
---
+++ snapshot_urls.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+from session import Session
+from cdx import search
+from settings import DEFAULT_USER_AGENT, DEFAULT_ROOT
+import argparse
+import logging
+
+ARCHIVE_TEMPLATE = "https://web.archive.org/web/{timestamp}/{url}"
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("url",
+        help="The URL of the resource you want to download.")
+
+    parser.add_argument("--from-date",
+        help="Timestamp-string indicating the earliest snapshot to download. Should take the format YYYYMMDDhhss, though you can omit as many of the trailing digits as you like. E.g., '201501' is valid.")
+
+    parser.add_argument("--to-date",
+        help="Timestamp-string indicating the latest snapshot to download. Should take the format YYYYMMDDhhss, though you can omit as many of the trailing digits as you like. E.g., '201604' is valid.")
+
+    parser.add_argument("--collapse",
+        help="An archive.org `collapse` parameter. Cf.: https://github.com/internetarchive/wayback/blob/master/wayback-cdx-server/README.md#collapsing",
+	action='append')
+
+    parser.add_argument("--user-agent",
+        help="The User-Agent header to send along with your requests to the Wayback Machine. If possible, please include the phrase 'waybackpack' and your email address. That way, if you're battering their servers, they know who to contact. Default: '{0}'.".format(DEFAULT_USER_AGENT),
+        default=DEFAULT_USER_AGENT)
+
+    parser.add_argument("--follow-redirects",
+        help="Follow redirects.",
+        action="store_true")
+
+    parser.add_argument("--uniques-only",
+        help="Download only the first version of duplicate files.",
+        action="store_true")
+
+    parser.add_argument("--max-retries",
+        help="How many times to try accessing content with 4XX or 5XX status code before skipping?",
+        type=int,
+        default=3)
+
+    parser.add_argument("--quiet",
+        action="store_true",
+        help="Don't log progress to stderr.")
+
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_args()
+
+    logging.basicConfig(
+        level=(logging.WARN if args.quiet else logging.INFO),
+        format="%(levelname)s:%(name)s: %(message)s"
+    )
+
+    session = Session(
+        user_agent=args.user_agent,
+        follow_redirects=args.follow_redirects,
+        max_retries=args.max_retries
+    )
+
+    snapshots = search(args.url,
+        session=session,
+        from_date=args.from_date,
+        to_date=args.to_date,
+        uniques_only=args.uniques_only,
+        collapse=args.collapse
+    )
+
+    urls = []
+    for snap in snapshots:
+        urls.append(
+            ARCHIVE_TEMPLATE.format(
+                timestamp=snap["timestamp"],
+                url=args.url,
+            )
+        )
+    print("\n".join(urls))
+
+
+if __name__ == "__main__":
+    main()
