cf6649a56639c82247fe254dca85ecd7bcf3263f
qiang <shijialee@gmail.com>
2022-04-06 01:22:44 +0000

    refactor

--- backpod/cli.py
+++ backpod/cli.py
@@ -22,7 +22,10 @@ def parse_args():
     #     help="The URL of the resource you want to download.")
 
     parser.add_argument("--user-agent",
-        help="The User-Agent header to send along with your requests to the Wayback Machine. If possible, please include the phrase 'waybackpack' and your email address. That way, if you're battering their servers, they know who to contact. Default: '{0}'.".format(DEFAULT_USER_AGENT),
+        help="The User-Agent header to send along with your requests to the "
+            "Wayback Machine. If possible, please include the phrase 'waybackpack' "
+            "and your email address. That way, if you're battering their servers,"
+            "they know who to contact. Default: '{0}'.".format(DEFAULT_USER_AGENT),
         default=DEFAULT_USER_AGENT)
 
     parser.add_argument("--max-retries",
@@ -60,52 +63,52 @@ def main(args, session, fh, first_request=False):
     root = etree.fromstring(res.content, parser=parser, base_url=res.url)
     selector = Selector(root=root, type='xml')
     if selector.xpath('//item').get() is None:
-        # no shows
+        # no episode
         return None
+
+    # get episode
+    episode_count = len(selector.xpath('//item'))
+    first_pub_date = selector.xpath('//item')[-1].xpath('.//pubDate/text()').get()
+    last_pub_date = selector.xpath('//item')[0].xpath('.//pubDate/text()').get()
+
+    log_msg = 'show count "{0}", first pub date "{1}, last "{2}"'
+    logger.info(log_msg.format(episode_count, first_pub_date, last_pub_date))
+
+    if first_request:
+        # get anything before <item>
+        index = res.text.find('<item>')
+        fh.write(res.text[0:index])
+        global close_tag
+        close_tag = True
+
+    # need this line, otherwise namespaces are added to each <item>
+    selector.remove_namespaces()
+    items = selector.xpath('//item').getall()
+    fh.write("\n".join(items))
+
+    # get wayback url
+    dt = get_date(first_pub_date)
+    if dt is None:
+        return
+
+    previous_date_dt = dt.date() - datetime.timedelta(days=1)
+    one_year_before_dt = dt.date() - datetime.timedelta(days=365)
+    args.from_date = one_year_before_dt.strftime('%Y%m%d')
+    args.to_date = previous_date_dt.strftime('%Y%m%d')
+    args.limit = '-2'
+    args.uniques_only = True
+    # reset to get the snapshot url for the original url
+    args.url = args.original_url
+    args.collapse = ['timestamp:8', 'digest']
+    logger.info('from {0} to {1}'.format(args.from_date, args.to_date))
+    urls = snapshot_urls(args, session)
+
+    if urls:
+        # we get wayback url here
+        args.url = urls[0]
+        main(args, session, fh)
     else:
-        # get shows
-        episode_count = len(selector.xpath('//item'))
-        first_pub_date = selector.xpath('//item')[-1].xpath('.//pubDate/text()').get()
-        last_pub_date = selector.xpath('//item')[0].xpath('.//pubDate/text()').get()
-
-        log_msg = 'show count "{0}", first pub date "{1}, last "{2}"'
-        logger.info(log_msg.format(episode_count, first_pub_date, last_pub_date))
-
-        if first_request:
-            # open tag
-            index = res.text.find('<item>')
-            fh.write(res.text[0:index])
-            global close_tag
-            close_tag = True
-
-        # need this line, otherwise namespaces are added to each <item>
-        selector.remove_namespaces()
-        items = selector.xpath('//item').getall()
-        fh.write("\n".join(items))
-
-        # get wayback url
-        dt = get_date(first_pub_date)
-        if dt is None:
-            return
-
-        previous_date_dt = dt.date() - datetime.timedelta(days=1)
-        one_year_before_dt = dt.date() - datetime.timedelta(days=365)
-        args.from_date = one_year_before_dt.strftime('%Y%m%d')
-        args.to_date = previous_date_dt.strftime('%Y%m%d')
-        args.limit = '-2'
-        args.uniques_only = True
-        # reset to get the snapshot url for the original url
-        args.url = args.original_url
-        args.collapse = ['timestamp:8', 'digest']
-        logger.info('from {0} to {1}'.format(args.from_date, args.to_date))
-        urls = snapshot_urls(args, session)
-
-        if urls:
-            # we get wayback url here
-            args.url = urls[0]
-            main(args, session, fh)
-        else:
-            return
+        return
 
 
 def get_date(datetime_str):
@@ -121,10 +124,11 @@ def get_date(datetime_str):
     try:
         dt = parse(datetime_str)
         return dt
-    except:
+    except Exception as inst:
         # print(f'error parsing {datetime_str}')
         # a value of 4000000000 will overflow, so we catch all here.
         # XXX override wrong format
+        logger.error(inst)
         logger.info('date format is wrong for {0}'.format(datetime_str))
         return None
 
@@ -192,4 +196,3 @@ if __name__ == "__main__":
 
         with conn:
             conn.execute('UPDATE feed SET status = ?, uuid = ? WHERE id = ?', (status, xml_file, job['id']))
-
